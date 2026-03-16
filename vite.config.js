372081e5c9212ea887b33feaff8eb3570e73f594
Peter Mina <peter.mina@e-sky.ca>
2026-03-16 19:44:00 +0200

    fix: remove base path, serve from root domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
   plugins: [tailwindcss(), react()],
   server: {
     port: 3000,
